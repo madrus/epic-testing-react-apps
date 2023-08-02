59e25ba56ca93a2d7dd55532fbf6f290f87d5319
Andre Roussakoff <madrus@gmail.com>
2023-08-02 19:55:14 +0200

    finish lesson 2 main part

--- src/__tests__/exercise/02.js
+++ src/__tests__/exercise/02.js
@@ -2,9 +2,9 @@
 // http://localhost:3000/counter
 
 import * as React from 'react'
-import {act} from 'react-dom/test-utils'
-import {createRoot} from 'react-dom/client'
+import { act } from 'react-dom/test-utils'
 // 🐨 import the `render` and `fireEvent` utilities from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 import Counter from '../../components/counter'
 
 // NOTE: this is a new requirement in React 18
@@ -13,29 +13,19 @@ import Counter from '../../components/counter'
 // 💣 so you can now delete this!
 global.IS_REACT_ACT_ENVIRONMENT = true
 
-// 💣 remove this. React Testing Library does this automatically!
-beforeEach(() => {
-  document.body.innerHTML = ''
-})
-
 test('counter increments and decrements when the buttons are clicked', () => {
-  // 💣 remove these two lines, React Testing Library will create the div for you
-  const div = document.createElement('div')
-  document.body.append(div)
-
   // 🐨 swap createRoot and root.render with React Testing Library's render
   // Note that React Testing Library's render doesn't need you to pass a `div`
   // so you only need to pass one argument. render returns an object with a
   // bunch of utilities on it. For now, let's just grab `container` which is
   // the div that React Testing Library creates for us.
   // 💰 const {container} = render(<Counter />)
-  const root = createRoot(div)
-  act(() => root.render(<Counter />))
+	const { container } = render(<Counter />)
 
   // 🐨 instead of `div` here you'll want to use the `container` you get back
   // from React Testing Library
-  const [decrement, increment] = div.querySelectorAll('button')
-  const message = div.firstChild.querySelector('div')
+  const [decrement, increment] = container.querySelectorAll('button')
+  const message = container.firstChild.querySelector('div')
 
   expect(message.textContent).toBe('Current count: 0')
 
