0e19557451cd5bec661ec44c65aacdd106621a72
Andre Roussakoff <madrus@gmail.com>
2023-08-05 15:04:39 +0200

    chore: finish extra 2

---
+++ src/__tests__/exercise/04.extra-2.js
@@ -0,0 +1,52 @@
+// form testing
+// http://localhost:3000/login
+
+import * as React from 'react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { faker } from '@faker-js/faker'
+import Login from '../../components/login'
+
+function setup(jsx) {
+  return {
+    user: userEvent.setup(),
+    // Import `render` from the framework library of your choice.
+    // See https://testing-library.com/docs/dom-testing-library/install#wrappers
+    ...render(jsx),
+  }
+}
+
+function buildLoginForm() {
+	return {
+		username: faker.internet.userName(),
+		password: faker.internet.password(),
+	}
+}
+
+test('submitting the form calls onSubmit with username and password', async () => {
+  //	Jest has built-in "mock" function APIs. Rather than creating the `submittedData`
+  //	variable, try to use a mock function and assert it was called correctly:
+  //
+  //	- 📜 `jest.fn()`: https://jestjs.io/docs/en/mock-function-api
+  //	- 📜 `toHaveBeenCalledWith`: https://jestjs.io/docs/en/expect#tohavebeencalledwitharg1-arg2-
+  const { username, password } = buildLoginForm()
+  const handleSubmit = jest.fn()
+
+  const { user } = setup(<Login onSubmit={handleSubmit} />)
+
+  await userEvent.type(screen.getByLabelText(/username/i), username)
+  await userEvent.type(screen.getByLabelText(/password/i), password)
+  await user.click(screen.getByRole('button', { name: /submit/i }))
+
+  //screen.debug // if necessary
+
+  // assert that handleSubmit has been called correctly
+  expect(handleSubmit).toHaveBeenCalledWith({ username, password })
+  // and only once
+  expect(handleSubmit).toHaveBeenCalledTimes(1)
+})
+
+/*
+eslint
+  no-unused-vars: "off",
+*/
